1d1a68ecb3ca712e991d8503ffd410b8fdc22696
Robert Espinoza <robert.espinoza@agilebits.com>
2022-08-03 23:16:52 -0700

    add to expected_default_fields

--- tests/base.py
+++ tests/base.py
@@ -869,7 +869,12 @@ class GoogleAdsBase(unittest.TestCase):
                 'click_type',
                 'clicks',
                 'interactions',
-            }
+            },
+            'location_performance_report': {
+                'campaign_criterion_location',
+                'clicks',
+                'impressions',
+            },
         }
     def assertIsDateFormat(self, value, str_format):
         """
