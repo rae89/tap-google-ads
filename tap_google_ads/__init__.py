13dd032f18978bfc2f1246be7455a4f13752ab02
Ella Wherritt <ella.wherritt@agilebits.com>
2024-06-06 21:08:20 -0600

    Revert "Merge pull request #9 from rae89/robert/googleads-service-account-auth"
    This reverts commit 55b962870d96d20e92d5074ab4edea956d7d7915, reversing
    changes made to 73f0c3986c8e3ebaea1f0e3c3c0be64b1d75e340.

--- tap_google_ads/__init__.py
+++ tap_google_ads/__init__.py
@@ -12,10 +12,13 @@ LOGGER = singer.get_logger()
 
 REQUIRED_CONFIG_KEYS = [
     "start_date",
+    "oauth_client_id",
+    "oauth_client_secret",
+    "refresh_token",
+    "customer_ids",
     "developer_token",
-    "impersonated_email",
-    "auth_method"
 ]
+
 
 def main_impl():
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
